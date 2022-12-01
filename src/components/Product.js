561a137411a4c79a23cdb29e68cc4409d65d61b8
Saxar88 <109401481+Saxar88@users.noreply.github.com>
2022-12-01 18:11:33 +0200

    Update Product.js

--- src/components/Product.js
+++ src/components/Product.js
@@ -11,10 +11,10 @@ function Product({id, image, title, author, rating, price, coverType}) {
 				id: id,
 				image: image,
 				title: title,
-				author: author,
+				author: author ? author : "N/A",
 				rating: rating,
 				price: price,
-				coverType: coverType,
+				coverType: coverType ? coverType : "N/A",
 			},
 		});
 	};
