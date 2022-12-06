bc472c680272ed40b0e70127cd93fdd748f98c54
Saxar88 <109401481+Saxar88@users.noreply.github.com>
2022-12-06 09:18:48 +0200

    Add files via upload

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,20 +5,20 @@ import Product from "../components/Product";
 import data from "../data";
 
 function Home() {
-  const products = data.map((item) => {
-    return <Product key={item.id} {...item} />;
-  });
+	const products = data.map((item) => {
+		return <Product key={item.id} {...item} />;
+	});
 
-  return (
-    <div className="home">
-      <div className="home--container">
-        <div className="home--image">
-          <ImageSlider />
-        </div>
-        <div className="home--products">{products}</div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="home">
+			<div className="home--container">
+				<div className="home--image">
+					<ImageSlider />
+				</div>
+				<div className="home--products">{products}</div>
+			</div>
+		</div>
+	);
 }
 
 export default Home;
